02ee1aee69a8b4bfe42465ba6453443e12459a25
charles <charles@computerwebservices.net>
2016-12-31 00:43:21 -0500

    added chat service and object message

--- app/signalR/chat.component.ts
+++ app/signalR/chat.component.ts
@@ -1,60 +1,33 @@
 import { Component, OnInit  } from '@angular/core';
 import { JQueryService } from './../jquery.service';
-
+import { ChatService, MsgClass } from './chat.service';
+ 
 @Component ({
 	selector: 'chat',
 	moduleId: module.id.toString(),
     templateUrl: './chat.html'    
 })
-export class MsgClass {
-    message: string;
-    name: string;
-}
+
 
 export class ChatComponent implements OnInit { 
     message: string = "";
     messages: Array<string>;
-    name: string;
-
-    msgComp: MsgClass;
+    name: string;   
     msgcomps: Array<MsgClass>;
-    chatConn: any;
-    constructor(public jq: JQueryService) {
-        this.messages = new Array<string>();
-        this.msgcomps = new Array<MsgClass>();
-        this.msgComp = new MsgClass();
+    constructor(private chatservice: ChatService) {
+        this.messages = new Array<string>();       
     }
     SendSignal(): void {
-        this.chatConn.server.send(this.name, this.message);
+        this.chatservice.sendChat(this.name, this.message);
         console.log("trying to send");
     }   
-    pushMessage(message: string): void {
-        console.log(message);
-        //this.messages.push(message);
-    }
+   
     ngOnInit(): void {
         this.name = " I AMTESTING";
         this.messages = new Array<string>();
         let myms = this.messages;
-        let myf = function (message: string) {
-            myms.push(message);
-        }
-        if (this.jq.JQueryOK) {
-            console.log('start init comp jq ok');  
-            let $ = this.jq.JQuery;
-            this.chatConn = $.connection.octo2Hub;
-           
-            // Create a function that the hub can call to broadcast messages.
-            this.chatConn.client.broadcastMessage = function (name: string, message: string) {                               
-                //console.log(message);
-                myf(message);
-                //this.pushMessage(message);
-            };
-            $.connection.hub.start().done(function () {
-                console.log("connection to hub done");
-             }) ;
-        }           
-    }	
-    
+        this.msgcomps = this.chatservice.msgcomps;
+        this.chatservice.start();
+    }	 
 
  }
---
+++ app/signalR/chat.service.ts
@@ -0,0 +1,51 @@
+﻿import { Injectable, Inject } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { JQueryService } from './../jquery.service';
+
+
+export class MsgClass {
+    constructor(public name: string = "",public message: string="" ) { } 
+}
+
+@Injectable()
+export class ChatService {
+    msgComp: MsgClass;
+    msgcomps: Array<MsgClass>;
+    chatConn: any;
+    hubOK: boolean;
+    constructor(private jq: JQueryService) {
+        this.hubOK = false;
+        this.msgcomps = new Array<MsgClass>();
+        this.msgComp = new MsgClass();        
+        if (this.jq.JQueryOK) {
+            let $ = this.jq.JQuery;
+            console.log('start init comp jq ok');
+            this.chatConn = $.connection.octo2Hub;            
+        }
+    }
+    start(): void {
+        if (this.jq.JQueryOK) {
+            let $ = this.jq.JQuery;
+            // Create a function that the hub can call to broadcast messages.
+            let myms = this.msgcomps;
+            let myf = function (name: string, message: string) {
+                myms.push(new MsgClass(name, message));
+            }
+            if (this.chatConn) {
+                this.chatConn.client.broadcastMessage = function (name: string, message: string) {
+                    myf(name, message);
+                };
+                $.connection.hub.start().done(function () {
+                    console.log("connection to hub done");
+                    this.hubOK = true;
+                });
+            }
+        }     
+    }
+    sendChat(name: string, message: string): void  {
+        if (this.hubOK)
+            this.chatConn.server.send(name, message);
+    }
+     
+}
