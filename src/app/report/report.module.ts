83bef62da7cb3d9b19cbc03d98312df6c272e64e
charles <charles.daniel@computerwebservices.net>
2017-01-05 11:56:31 -0500

    Added report module added vs project

---
+++ src/app/report/report.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideosComponent } from './videos/videos.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [VideosComponent]
+})
+export class ReportModule { }
---
+++ src/app/report/videos/video.ts
@@ -0,0 +1,22 @@
+export  enum patact {
+    unassigned,
+    patient,
+    actor,
+    unknown
+}
+export class Language {
+    public languagename: string;
+    public id: number;
+}
+export class Video {
+    constructor(
+        public id: number,
+        public filename: string,
+        public subjectname: string,
+        public patientact: patact,
+        public videoid: number,
+        public language: Language,        
+        public transcripts: Language[] 
+    ) { }
+     
+}
