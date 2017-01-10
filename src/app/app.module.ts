16cb875985eae908aae3bf54a9d00f01d8b2459f
charles <charles@computerwebservices.net>
2017-01-10 00:20:59 -0500

    video listing to api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { KnightModule } from './knight/knight.module';
 import { TicTacModule } from './tictac/tictac.module';
 import { SignalRModule } from './signal-r/signal-r.module';
+import { ReportModule } from './report/report.module';
+
 import { JQueryService, WindowExtend, JQMinSpec } from './jquery.service';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about.component';
@@ -14,7 +16,8 @@ import { AboutComponent } from './about.component';
 @NgModule ({ imports: [CommonModule, BrowserModule, FormsModule, HttpModule, 
 		KnightModule, 
 		TicTacModule, 
-		SignalRModule, 
+        SignalRModule, 
+        ReportModule,
 		AppRoutingModule, ], 
 		declarations: [  AppComponent, AboutComponent ], 
 		providers: [JQueryService, { provide: WindowExtend, useValue: window }], bootstrap: [AppComponent] 
