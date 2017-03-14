229a57a1d48e963dd010ba8e6b5ef365533b18c5
Charles Don <Charles Don>
2017-03-13 23:22:52 -0400

    fix comp test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,16 +9,14 @@ import {RouterOutlet } from './testing/router-stub.directive';
 import  { ActivatedRouteStub } from './testing/activated-route-stub';
 import {RouterStub  } from './testing/router-stub';
 import { WatcherService } from './watcher.service'; 
-
+import { Router, ActivatedRoute } from "@angular/router";
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;	
   let comp: AppComponent;
   let de: DebugElement;
   let el: HTMLElement;  
-  let activatedRouteStub=new ActivatedRouteStub();
-  let routerStub=new RouterStub();
-  let watcherServiceStub=new WatcherService();  
+   
   // async beforeEach
    beforeEach(async(() => {  
 	  TestBed.configureTestingModule({
@@ -28,34 +26,34 @@ describe('AppComponent', () => {
 			RouterOutlet
 			],
 		providers: [ 
-				{provide: ActivatedRoute, useValue: ActivatedRouteStub() },
-				{provide: Router, useValue: new RouterStub() },
+            { provide: ActivatedRoute, useClass: ActivatedRouteStub },
+            { provide: Router, useClass: RouterStub },
 				WatcherService
 			]	
 	  }).compileComponents();	
   }));
-  /*
-  beforeEach(() => {    
+    beforeEach(() => {    
 	fixture = TestBed.createComponent(AppComponent);
 	comp=fixture.componentInstance;
-	de=fixture.debugElement.query(By.css('h1'));
+    de = fixture.debugElement.query(By.css('a.navbar-brand'));
 	el=de.nativeElement;
     
   });
- */
+ 
 
   it('should create the app',async(() => {    
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    //const fixture = TestBed.createComponent(AppComponent);
+   // const app = fixture.debugElement.componentInstance;
+      expect(comp).toBeTruthy();
   }));
-  /*
+  
   it(`should have as title 'app works!'`, async(() => {   
     expect(comp.title).toEqual('app works!');
   }));
 
   it('should render title in a h1 tag', async(() => {       
-    expect(de.textContent).toContain('app works!');
+      console.log(el);
+      expect(el).toBeTruthy();
   }));
-  */
+ 
 });
